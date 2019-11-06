8b3c44bf71946763aa786c1180ab8e51227aecf0
1432junaid <1432junaid@gmail.com>
2019-11-06 12:28:55 +0530

    a

--- string/strdemo1.cpp
+++ string/strdemo1.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 #include<string>
+#include<cctype>		//to use char functions like isalpha,isalnum,islower,idnumber,isdigit,isxnumber,isgrap,isascii,toupper
+
 using namespace std;
 //using std::string;
+//using std::string::size_type;
+
 
 int main(){
 	string s1;
@@ -22,6 +26,24 @@ int main(){
 	cout<<(s3==s2)<<endl;
 	cout<<(s1<s2)<<endl;
 	bool b=(s2>s3);
-	cout<<b<<endl;
+	cout<<b<<endl<<endl<<endl;
+//	for(string::size_type i=0; i<s2.size;i++){
+//		cout<<s2[i]<<endl;
+//	}
+
+	string collage("Integral");
+	for(string::size_type i=0;i<collage.size();i++){   //intilize i with int 
+		cout<<collage[i]<<endl;
+	}
+
+	string s5("ab3cde");
+	for(string::size_type i=0;i<s5.size();i++){
+		if(isalpha(s5[i])){
+			s5[i]=toupper(s5[i]);
+		}
+	}
+	cout<<s5<<endl;
+
+
 	return 0;
 }
